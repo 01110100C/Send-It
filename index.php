097693007aab9f731d4179edbf2ef249a3cd1ac8
Tayler Caufield <taylercaufield@gmail.com>
2026-04-22 15:48:22 -0700

    Created log in page and updated css

--- index.php
+++ index.php
@@ -7,17 +7,15 @@
     <title>Send It Main Page</title>
 </head>
 <body>
-    <main>
     <h1> Send It </h1>
+    <br>
         <div class="description">
-        <P> Welcome to Send It, your go-to platform for sharing and tracking climbs! </p>
+        <p> Welcome to Send It, your go-to platform for sharing and tracking climbs! </p>
         <p> To get started, please register for an account or log in if you already have one. </p>
         </div>
                  <div class="links">
                      <a href="registration.php"> Register </a>
                      <a href="login.php"> Log In </a>
                     </div>
-     
-</main>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" href="styles.css" />
-    <script src="script.js"></script>
     <meta charset="UTF-8" />
     <title>Log In Page </title>
+    <link rel="stylesheet" href="styles.css" />
+    <script src="script.js"></script>
 </head>
 <body>
-    <main>
-
-    </main>
-    
+    <div class="main">
+        <h1>Welcome Back</h1>
+        <br>
+        <h3>Please log in to your account</h3>
+                <form action="">
+                    <label for="username">Username:</label>
+                    <input type="text" placeholder="Enter your username" required />
+                    <br>
+                    <label for="password">Password:</label>
+                    <input type="password" placeholder="Enter your password" required />
+                    <div class="wrap">
+                    <button type="submit">Log In</button>
+                    </div>
+                </form>
+            </div>
+</div>
+<a href="index.php"> Go Back </a>
 </body>
 </html>
