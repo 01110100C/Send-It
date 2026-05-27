a16e4b209c6d77e13929a4442fe19c9bd4444481
Tayler Caufield <taylercaufield@gmail.com>
2026-05-27 15:13:38 -0700

    Working on ranked page

--- rank.php
+++ rank.php
@@ -16,5 +16,47 @@
             <li><a href="rank.php">Ranked</a></li>
         </ul>
     </nav>
+<br>
+<br>
+<br>
+
+<h1> Climbing Rankings </h1>
+<p>Climbers ranks are updated daily based on the grade level they are sending and how many sends they have. </p>
+
+<div class="layout"> 
+
+    <div class="rank-tabs" id="rankTabs"></div>
+
+
+    <div cladd="hero-card" id="heroCard"> 
+
+    </div>
+
+    <div class="stat-grid" id="statGrid">
+
+    </div>
+
+    <div class="progeress-row" id="progressRow">
+
+    </div>
+
+    <div class=section-title>Leaderboard</div>
+    <table class="lb-table">
+        <thead>
+        <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Grade</th>
+            <th>Sends</th>
+        </tr>
+       </thead>
+         <tbody>
+
+
+         </tbody>
+    </table>
+
+</div>
+
 </body>
 </html>
